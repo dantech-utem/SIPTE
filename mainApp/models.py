418da96fed85f0e09b6c4672452e45f92ac33461
JobRodriguez <a20210080@utem.edu.mx>
2024-06-20 20:24:24 -0600

    Creacion de modelos equipo config

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class TipoUsuario(models.Model):
+    tipo = models.CharField(max_length=100)
+    
+class Usuarios(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    noControl = models.CharField(max_length=20)
+    correo = models.CharField(max_length=100)
+    contrasena = models.CharField(max_length=100)
+    tipo = models.ForeignKey(TipoUsuario, on_delete=models.CASCADE)
+    grupo = models.CharField(max_length=20)

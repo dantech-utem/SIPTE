8d534ee1d7fe283506959551c39252f44b1a9990
xManueldyx <105610483+xManueldyx@users.noreply.github.com>
2024-06-25 16:07:13 -0600

    acomodo de rutas templates, css ademas de implementacion de menu lateral en el modulo de canalizacion

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -7,5 +7,13 @@ from . import views
 urlpatterns = [
     path('', inicio.as_view(), name="inicio"),
     path('test/', loginTest.as_view(), name="test"),
-
+    path('Dashboard', canalizacionIndex.as_view(), name="Dashboard"),
+    path('Calendario', canalizacionCalendario.as_view(), name="Calendario"),
+    path('CompletarSesion', canalizacionCompletarSesion.as_view(), name="CompletarSesion"),
+    path('Expediente', canalizacionExpedientes.as_view(), name="Expediente"),
+    path('FormBaja', canalizacionBajas.as_view(), name="FormBaja"),
+    path('FormCanalizar', canalizacionFormCanalizar.as_view(), name="FormCanalizar"),
+    path('FormCerrarTutorias', canalizacionFormCerrarTutorias.as_view(), name="FormCerrarTutorias"),
+    path('Reportes', canalizacionReportes.as_view(), name="Reportes"),
+    path('ResultadosCanalizacion', canalizacionResultadosCanalizacion.as_view(), name="ResultadosCanalizacion"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- mainApp/views.py
+++ mainApp/views.py
@@ -9,3 +9,39 @@ class inicio(View):
 class loginTest(View):
    def get(self, request):
       return render(request,'test/prueba.html')
+   
+class canalizacionIndex(View):
+   def get(self, request):
+      return render(request,'Canalizacion/index.html')
+
+class canalizacionCalendario(View):
+   def get(self, request):
+      return render(request,'Canalizacion/calendario.html')
+   
+class canalizacionCompletarSesion(View):
+   def get(self, request):
+      return render(request,'Canalizacion/completarSesion.html')
+   
+class canalizacionExpedientes(View):
+   def get(self, request):
+      return render(request,'Canalizacion/expediente.html')
+   
+class canalizacionBajas(View):
+   def get(self, request):
+      return render(request,'Canalizacion/formBaja.html')
+   
+class canalizacionFormCanalizar(View):
+   def get(self, request):
+      return render(request,'Canalizacion/formCanalizar.html')
+   
+class canalizacionFormCerrarTutorias(View):
+   def get(self, request):
+      return render(request,'Canalizacion/formCerrarTutorias.html')
+   
+class canalizacionReportes(View):
+   def get(self, request):
+      return render(request,'Canalizacion/reportes.html')
+   
+class canalizacionResultadosCanalizacion(View):
+   def get(self, request):
+      return render(request,'Canalizacion/resultadosCanalizacion.html')

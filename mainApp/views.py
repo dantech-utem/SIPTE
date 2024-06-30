7c298b67d94a8ed45f855e4e13eeae2ad330a065
xManueldyx <105610483+xManueldyx@users.noreply.github.com>
2024-06-30 14:47:51 -0600

    Crud bajaAlumnos y Canalizaciones(Canalizacion)

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Canalizacion, BajaAlumnos,Canalizar
 from django.views import View
+from django.views.decorators.http import require_http_methods
 
 # Create your views here.
 class inicio(View):
@@ -26,13 +28,39 @@ class canalizacionExpedientes(View):
    def get(self, request):
       return render(request,'Canalizacion/expediente.html')
    
+   #CRUD BAJAS CANALIZACION
 class canalizacionBajas(View):
    def get(self, request):
       return render(request,'Canalizacion/formBaja.html')
    
+def canalizacionBajaAlumno(request):
+   if request.method == "POST":
+      tipo = request.POST['tipo']
+      observaciones = request.POST['observaciones']
+      motivo = request.POST['motivo']
+      # Crear un nuevo registro en la base de datos
+      Baja  = BajaAlumnos.objects.create(tipo=tipo, observaciones=observaciones, motivo=motivo)
+      Baja.save()
+      # Redirigir a una página de éxito o de lista después de la creación
+      return redirect('Dashboard')
+   #TERMINA CRUD BAJAS CANALIZACION
+   
+   #CRUD CANALIZAR CANALIZACION
 class canalizacionFormCanalizar(View):
    def get(self, request):
       return render(request,'Canalizacion/formCanalizar.html')
+   
+def canalizacionFormCanalizarAlumno(request):
+   if request.method == "POST":
+      area = request.POST['area']
+      observaciones = request.POST['observaciones']
+      motivo = request.POST['motivo']
+      # Crear un nuevo registro en la base de datos
+      canalizacion  = Canalizar.objects.create(area=area, observaciones=observaciones, motivo=motivo)
+      canalizacion.save()
+      # Redirigir a una página de éxito o de lista después de la creación
+      return redirect('Dashboard')
+   #TERMINA CRUD CANALIZAR CANALIZACION
    
 class canalizacionFormCerrarTutorias(View):
    def get(self, request):

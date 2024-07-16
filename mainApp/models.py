edcd89423d0be162fae24728de2cfc3aaa999eb1
DianaBRamirez <dianabramirez29@gmail.com>
2024-07-15 22:09:24 -0600

    rama documentos

--- mainApp/models.py
+++ mainApp/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 class TipoUsuario(models.Model):
     tipo = models.CharField(max_length=100)
+    def __str__(self):
+        return self.tipo
     
 class Usuarios(models.Model):
     User = models.OneToOneField(User, on_delete=models.CASCADE)
